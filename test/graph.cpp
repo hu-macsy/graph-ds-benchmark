bd7ad916f9490d5607a7491b2ade1877d04f9805
vivid <2944106+c-bebop@users.noreply.github.com>
2022-12-16 10:52:42 +0100

    Test vertex_count().

--- test/graph.cpp
+++ test/graph.cpp
@@ -1,8 +1,47 @@
 #include <catch2/catch_test_macros.hpp>
 
+// https://networkrepository.com/bio-celegans.php
+static std::string const graph_path =
+#ifdef GDSB_TEST_GRAPH_DIR
+    std::string(GDSB_TEST_GRAPH_DIR) + "/";
+#else
+    "test/graphs/";
+#endif
+
+static std::string unweighted_temporal_graph{ "reptilia-tortoise-network-pv.edges" };
+
 #include <gdsb/graph.h>
+#include <gdsb/graph_io.h>
 
 using namespace gdsb;
+
+TEST_CASE("Vertex Counting")
+{
+    SECTION("Using type Edges")
+    {
+        Edges edges;
+        auto emplace = [&](Vertex u, Vertex v, Weight w) { edges.push_back(Edge{ u, Target{ v, w } }); };
+
+        std::ifstream graph_input(graph_path + unweighted_temporal_graph);
+
+        read_undirected_graph<Vertex, decltype(emplace)>(graph_input, true, std::move(emplace), []() { return 1.f; });
+
+        CHECK(vertex_count(edges) == 36);
+    }
+
+
+    SECTION("Using 64bit typed Edges")
+    {
+        Edges64 edges;
+        auto emplace = [&](Vertex64 u, Vertex64 v, Weight w) { edges.push_back(Edge64{ u, Target64{ v, w } }); };
+
+        std::ifstream graph_input(graph_path + unweighted_temporal_graph);
+
+        read_undirected_graph<Vertex64, decltype(emplace)>(graph_input, true, std::move(emplace), []() { return 1.f; });
+
+        CHECK(vertex_count(edges) == 36);
+    }
+}
 
 TEST_CASE("Graph")
 {
