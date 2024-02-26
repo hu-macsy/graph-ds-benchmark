5f17fee16a5951ca3462a9915cc64de3654a7904
Jones <2944106+c-bebop@users.noreply.github.com>
2024-02-26 17:10:23 +0100

    Test MPI file opening.

--- include/gdsb/mpi_graph_io.h
+++ include/gdsb/mpi_graph_io.h
@@ -2,6 +2,7 @@
 
 #include <mpi.h>
 
+#include <filesystem>
 #include <stdexcept>
 
 namespace gdsb
@@ -9,11 +10,15 @@ namespace gdsb
 namespace mpi
 {
 
-inline MPI_File open_file(std::string const& file_name)
+inline MPI_File open_file(std::filesystem::path const& file_path)
 {
     MPI_File f;
-    int error = MPI_File_open(MPI_COMM_WORLD, file_name.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &f);
-    assert(error == MPI_SUCCESS);
+    int error = MPI_File_open(MPI_COMM_WORLD, file_path.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &f);
+
+    if (error != MPI_SUCCESS)
+    {
+        throw std::runtime_error("Could not open file using MPI routines.");
+    }
 
     return f;
 }
--- test/mpi_graph_io_tests.cpp
+++ test/mpi_graph_io_tests.cpp
@@ -1,22 +1,46 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <catch2/reporters/catch_reporter_event_listener.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
+
 #include "test_graph.h"
 
 #include <gdsb/mpi_graph_io.h>
 
-using namespace gdsb;
+#include <filesystem>
+
+class mpiInitListener : public Catch::EventListenerBase
+{
+public:
+    using Catch::EventListenerBase::EventListenerBase;
+
+    void testRunStarting(Catch::TestRunInfo const&) override
+    {
+        constexpr int required_thread_support = MPI_THREAD_MULTIPLE;
+        int provided_thread_support = required_thread_support;
+
+        int dumy_argc = 0;
+        char** dummy_argv;
+        MPI_Init_thread(&dumy_argc, &dummy_argv, required_thread_support, &provided_thread_support);
+        assert(required_thread_support == provided_thread_support);
+    }
+
+    void testRunEnded(Catch::TestRunStats const& testRunStats) override { MPI_Finalize(); }
+};
+
+CATCH_REGISTER_LISTENER(mpiInitListener)
 
 TEST_CASE("Open MPI File")
 {
-    constexpr int required_thread_support = MPI_THREAD_MULTIPLE;
-    int provided_thread_support = required_thread_support;
+    SECTION("Does not throw using valid path.")
+    {
+        std::filesystem::path file_path(graph_path + unweighted_directed_graph_enzymes);
+        CHECK_NOTHROW(gdsb::mpi::open_file(file_path));
+    }
 
-    int dumy_argc = 0;
-    char** dummy_argv;
-    MPI_Init_thread(&dumy_argc, &dummy_argv, required_thread_support, &provided_thread_support);
-    assert(required_thread_support == provided_thread_support);
-
-    SECTION("Does not throw") { CHECK_NOTHROW(gdsb::mpi::open_file(graph_path + unweighted_directed_graph_enzymes)); }
-
-    MPI_Finalize();
+    SECTION("Throws using invalid path.")
+    {
+        std::filesystem::path file_path(unweighted_directed_graph_enzymes);
+        CHECK_THROWS(gdsb::mpi::open_file(file_path));
+    }
 }
