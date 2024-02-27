30b4994e470c5442cbb686a7b705be58a34ba9f5
Jones <2944106+c-bebop@users.noreply.github.com>
2024-02-27 15:31:20 +0100

    Delete file after test!

--- test/graph_output_tests.cpp
+++ test/graph_output_tests.cpp
@@ -5,12 +5,17 @@
 #include <gdsb/graph_input.h>
 #include <gdsb/graph_output.h>
 
+#include <cstdio>
 #include <filesystem>
 #include <fstream>
 
-TEST_CASE("Write file")
+TEST_CASE("open_binary_file")
 {
     std::filesystem::path file_path{ graph_path + "write_test.gdsb" };
     std::ofstream out_file = gdsb::open_binary_file(file_path);
     CHECK(out_file.is_open());
+    CHECK(out_file.good());
+
+    // Delete the file!
+    REQUIRE(std::remove(file_path.c_str()) == 0);
 }
