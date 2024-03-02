0196aa0b7549cc586bb0891d69ea4a2ccebe68df
Jones <2944106+c-bebop@users.noreply.github.com>
2024-03-02 11:39:34 +0100

    Skeleton for MPI file open and read routines.

--- include/gdsb/mpi_graph_io.h
+++ include/gdsb/mpi_graph_io.h
@@ -23,15 +23,56 @@ inline MPI_File open_file(std::filesystem::path const& file_path)
     return f;
 }
 
-// inline get_line(MPI_File file)
-// {
-//     char line[1024];
+inline BinaryGraphHeaderMetaDataV1 read_binary_graph_header(MPI_File input)
+{
+    BinaryGraphHeaderIdentifier id;
 
-//     while (fgets(line, 1024, fp))
-//     {
-//         printf("%s\n", line);
-//     }
-// }
+    MPI_Status status;
+    int error = MPI_File_read_all(input, &id, sizeof(BinaryGraphHeaderIdentifier), MPI_CHAR, &status);
+    if (error != MPI_SUCCESS)
+    {
+        throw std::runtime_error("Could not read header identifier.");
+    }
+
+    GraphParameters<FileType::binary> graph_parameters;
+
+    switch (id.version)
+    {
+    case 1:
+    {
+        if (!std::strcmp(id.identifier, "GDSB"))
+        {
+            throw std::logic_error(std::string("Binary graph file has wrong identifier: ") + std::string(id.identifier));
+        }
+
+        BinaryGraphHeaderMetaDataV1 meta_data;
+        error = MPI_File_read_all(input, &meta_data, sizeof(BinaryGraphHeaderMetaDataV1), MPI_CHAR, &status);
+        if (error != MPI_SUCCESS)
+        {
+            throw std::runtime_error("Could not read meta data.");
+        }
+
+        return meta_data;
+    }
+    default:
+        throw std::logic_error("Binary graph version not supported: " + std::to_string(id.version));
+    }
+}
+
+template <typename ReadF> std::tuple<Vertex64, uint64_t> read_binary_graph(MPI_File input, ReadF&& read)
+{
+    BinaryGraphHeaderMetaDataV1 data = read_binary_graph_header(input);
+
+    bool continue_reading = true;
+    uint64_t edge_count = data.edge_count;
+
+    for (uint64_t e = 0; e < edge_count && input.is_open() && continue_reading; ++e)
+    {
+        continue_reading = read(input);
+    }
+
+    return std::make_tuple(data.vertex_count, data.edge_count);
+}
 
 // template <typename EmplaceF, typename GraphParameters = GraphParameters<FileType::edge_list>, typename Timestamp = uint64_t>
 // read_graph_partition_mpi(MPI_File file, EmplaceF&& emplace, uint32_t const partition_id, uint32_t const partition_count, uint64_t const edge_count)
