40f50df22367dbea4589f5e328df1566108546c9
Jones <2944106+c-bebop@users.noreply.github.com>
2024-06-12 15:18:26 +0200

    Global enzymes edge count value for tests.

--- test/test_graph.h
+++ test/test_graph.h
@@ -18,3 +18,4 @@ static std::string unweighted_directed_graph_enzymes_bin{ "ENZYMES_g1.bin" };
 static std::string undirected_unweighted_soc_dolphins{ "soc-dolphins.mtx" };
 
 constexpr uint32_t enzymes_g1_vertex_count = 38;
+constexpr uint32_t enzymes_g1_edge_count = 168;
