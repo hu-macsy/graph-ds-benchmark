1a43604d4247224dbcffe30709e6707b154519dd
Jones <2944106+c-bebop@users.noreply.github.com>
2024-10-03 17:05:57 +0200

    Leave a comment on read_ulong() and read_float() behaviour checking for errno.

--- src/graph_input.cpp
+++ src/graph_input.cpp
@@ -11,6 +11,11 @@
 namespace gdsb
 {
 
+// For both read_ulong() and read_float() we're using std::strtoul() and
+// std::strtof() respectively setting errno to ERANGE in case of an error. Clang
+// does not support the use of ERANGE. We therefore ignore the error check for
+// Clang. Please see https://en.cppreference.com/w/cpp/string/byte/strtof for
+// more information.
 unsigned long read_ulong(char const* source, char** end)
 {
     constexpr int numerical_base = 10;
